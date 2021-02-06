2bef93308a03a188fff23cd2acfff81711e4ed02
Abdulkhakim Magomedov <thevckit@gmail.com>
2021-02-06 18:02:07 +0300

    Показывает, сколько записей в таблице

--- WPF/DemoTest/WpfApp6/WpfApp6/Views/Pages/UserPage/DataViewPage.xaml.cs
+++ WPF/DemoTest/WpfApp6/WpfApp6/Views/Pages/UserPage/DataViewPage.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using WpfApp6.Context;
 using WpfApp6.Model;
 
@@ -23,12 +12,14 @@ namespace WpfApp6.Views.Pages.UserPage
     /// </summary>
     public partial class DataViewPage : Page
     {
+        public string counts { get; set; }
         public ObservableCollection<Service> Services { get; set; }
+
         public DataViewPage()
         {
             InitializeComponent();
+            counts = DbContextObject.db.Service.Count().ToString();
             Services = new ObservableCollection<Service>(DbContextObject.db.Service);
-            var counts = DbContextObject.db.Service.Count().ToString();
             this.DataContext = this;
         }
     }
