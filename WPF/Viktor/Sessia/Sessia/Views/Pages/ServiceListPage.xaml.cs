6cf83a3565c17090edceebc2f4117a5f3a623aad
Domovski1 <624273242@mail.ru>
2021-02-09 12:01:48 +0300

    Проект, для исправления ошибки

---
+++ WPF/Viktor/Sessia/Sessia/Views/Pages/ServiceListPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Sessia.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sessia.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ServiceListPage.xaml
+    /// </summary>
+    public partial class ServiceListPage : Page
+    {
+        public ObservableCollection<Service> service { get; set; }
+        public ServiceListPage()
+        {
+            InitializeComponent();
+            service = new ObservableCollection<Service>(BaseClass.db.Service.ToList());
+            this.DataContext = this;
+
+            
+        }
+
+        private void CmbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+
+            if (CmbSort.Text == "по возрастанию")
+            {
+                ProductListView.ItemsSource = BaseClass.db.Service.OrderBy(x => x.Cost).ToList();
+            }
+            else if (CmbSort.Text == "по убыванию")
+            {
+                ProductListView.ItemsSource = BaseClass.db.Service.OrderByDescending(x => x.Cost).ToList();
+            }
+            else if (CmbSort.Text == "скидка 0-5%")
+            {
+                ProductListView.ItemsSource = BaseClass.db.Service.Where(x => x.Discount < 5).ToList();
+            }
+            else if (CmbSort.Text == "скидка 5-15%")
+            {
+                ProductListView.ItemsSource = BaseClass.db.Service.Where(x =>  x.Discount < 15).ToList();
+            }
+            else if (CmbSort.Text == "скидка 15-30%")
+            {
+                ProductListView.ItemsSource = BaseClass.db.Service.Where(x => x.Discount < 30).ToList();
+            }
+            else if (CmbSort.Text == "скидка 30-70%")
+            {
+                ProductListView.ItemsSource = BaseClass.db.Service.Where(x =>x.Discount < 70).ToList();
+            }
+            else if (CmbSort.Text == "скидка 70-100%")
+            {
+                ProductListView.ItemsSource = BaseClass.db.Service.Where(x => x.Discount > 70).ToList();
+            }
+            else if (CmbSort.Text == "Все")
+            {   
+                ProductListView.ItemsSource = BaseClass.db.Service.ToList();
+            }
+
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var DelService = (Service)ProductListView.SelectedItem;
+            BaseClass.db.Service.Remove(DelService);
+        }
+
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ProductListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            NavigationService.Navigate((Service)ProductListView.SelectedItem);
+
+        }
+    }
+}
---
+++ WPF/Viktor/Sessia/Sessia/Views/Pages/ServicePage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Win32;
+using Sessia.Base;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace Sessia.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ServicePage.xaml
+    /// </summary>
+    public partial class ServicePage : Page
+    {
+        public Service service { get; set; }
+        public ServicePage photo { get; set; }
+        public ServicePage(Service GetService)
+        {
+            InitializeComponent();
+            service = GetService;
+            this.DataContext = this;
+        }
+
+        private void BtnImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "(*.png); (*.jpg) | *.png; *.jpg";
+
+            if (file.ShowDialog() == true)
+            {
+                ImgBox.Source = new BitmapImage(new Uri(file.FileName));
+            }
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var IsUnique = BaseClass.db.Service.FirstOrDefault(x => x.Title == TxbTitle.Text);
+                if (service.ID == 0)
+                {
+                    if (IsUnique != null)
+                    {
+                        MessageBox.Show("Такая услуга имеется");
+                    }
+                    else
+                    {
+                        BaseClass.db.Service.Add(service);
+
+                        BaseClass.db.SaveChanges();
+                        MessageBox.Show("Всё сохранено");
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Возникло исключение");
+            }
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = "01234567890".IndexOf(e.Text) < 0;
+        }
+
+        private void MaxDuration(object sender, TextChangedEventArgs e)
+        {
+            int TimeLimit = int.Parse(TxbDuration.Text);
+            if (TimeLimit > 14400)
+            {
+                TxbDuration.Text = "14400";
+            }
+        }
+    }
+}
